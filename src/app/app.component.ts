1c1c8c3ff42b9fe5a7c7f38f1e3829e379c21f7a
Brunhilde <brandontu35@gmail.com>
2024-07-25 18:36:09 -0400

    feat added more styling changes, made functional remove button and remade cocktail display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,48 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
-import { HlmCaptionComponent, HlmTableComponent, HlmTdComponent, HlmThComponent, HlmTrowComponent } from '@spartan-ng/ui-table-helm';
-
+import {
+  HlmCaptionComponent,
+  HlmTableComponent,
+  HlmTdComponent,
+  HlmThComponent,
+  HlmTrowComponent,
+} from '@spartan-ng/ui-table-helm';
+import { BrnAccordionContentComponent } from '@spartan-ng/ui-accordion-brain';
+import {
+  HlmAccordionContentDirective,
+  HlmAccordionDirective,
+  HlmAccordionIconDirective,
+  HlmAccordionItemDirective,
+  HlmAccordionTriggerDirective,
+} from '@spartan-ng/ui-accordion-helm';
+import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReactiveFormsModule, CommonModule, HlmButtonDirective, HlmInputDirective, HlmTableComponent,HlmTdComponent,HlmThComponent,HlmTrowComponent,HlmCaptionComponent],
+  imports: [
+    RouterOutlet,
+    ReactiveFormsModule,
+    CommonModule,
+    HlmButtonDirective,
+    HlmInputDirective,
+    HlmTableComponent,
+    HlmTdComponent,
+    HlmThComponent,
+    HlmTrowComponent,
+    HlmCaptionComponent,
+    BrnAccordionContentComponent,
+    HlmAccordionDirective,
+    HlmAccordionItemDirective,
+    HlmAccordionTriggerDirective,
+    HlmAccordionContentDirective,
+    HlmAccordionIconDirective,
+    HlmIconComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-
 export class AppComponent {
   title = 'Make Do';
   curIngredientList: string[] = [];
@@ -38,27 +69,40 @@ export class AppComponent {
     }
   };
 
-  searchCocktail = async() =>  {
+  removeItem = (ingr: string) => {
+    console.log('CLICKED');
+    console.log(this.curIngredientList);
+    const index = this.curIngredientList.indexOf(ingr);
+    if (index > -1) {
+      this.curIngredientList.splice(index, 1);
+    }
+    console.log(this.curIngredientList);
+  };
+
+  searchCocktail = async () => {
     console.log('placeholder');
-    if(this.curIngredientList === undefined || this.curIngredientList.length === 0){
-      console.log("Add code to alert the user!!!");
+    if (
+      this.curIngredientList === undefined ||
+      this.curIngredientList.length === 0
+    ) {
+      console.log('Add code to alert the user!!!');
     } else {
-      const response = await fetch('/api/cocktailSearch',{
-        method: "POST",
+      const response = await fetch('/api/cocktailSearch', {
+        method: 'POST',
         body: JSON.stringify({ ingredientList: this.curIngredientList }),
       });
 
-      const resCocktail = await response.json()
+      const resCocktail = await response.json();
       this.curCockTailList = JSON.parse(resCocktail.body);
       console.log(this.curCockTailList);
     }
-    }
+  };
 }
 
 export interface cocktail {
-  ingredients: string[]
-  instructions: string
-  name: string
+  ingredients: string[];
+  instructions: string;
+  name: string;
 }
 
 const testHi = async () => {
